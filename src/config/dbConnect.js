9a8ad190cc77e200d7019d8cdec26e0af5ae2f83
dulcidio <dulcidio.sobrinho@gmail.com>
2023-08-08 20:54:59 -0300

    envio do projeto para o github

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://alura:vrdv4569@cluster0.cp6jaaz.mongodb.net/alura");
+mongoose.connect("mongodb+srv://seubanco:SUASENHA@cluster0.cp6jaaz.mongodb.net/seubanco");
 
 let db = mongoose.connection;
 
